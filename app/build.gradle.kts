120cb3b9abad6c320b98d71f39778c8078a31a85
The-LukeZ <contact.lukez@proton.me>
2026-02-14 22:12:42 +0100

    Refactor: Remove Android application module
    Deletes the Android application module, including the `AndroidManifest.xml` and related Gradle configurations. Updates the Gradle wrapper to version 9.3.1 and removes unnecessary Gradle properties. Adds standard `gradlew` and `gradlew.bat` wrapper scripts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.theLukeZ.fidoscanner"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.theLukeZ.fidoscanner"
@@ -31,11 +30,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    
+
     buildFeatures {
         viewBinding = true
     }
